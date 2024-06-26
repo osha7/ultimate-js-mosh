67c22302a836667c7ab25f187342a4d847a651e0
Osha Groetz <ogroetz@gmail.com>
2024-06-26 10:39:39 -0500

    working with modules

---
+++ circle.js
@@ -0,0 +1,14 @@
+const _radius = new WeakMap();
+
+class Circle {
+    constructor(radius) {
+        _radius.set(this, radius);
+    }
+
+    draw() {
+        console.log('Circle with radius ' + _radius.get(this));
+    }
+}
+
+// module.exports.Circle = Circle;
+module.exports = Circle; 
--- index.js
+++ index.js
@@ -1,2 +1,36 @@
 console.log('Hello World');
 
+// --------------------------------------------------------------------------------
+
+// ------> code for 'circle.js' module:
+
+// // const _radius = new WeakMap();
+
+// // class Circle {
+// //     constructor(radius) {
+// //         _radius.set(this, radius);
+// //     }
+
+// //     draw() {
+// //         console.log('Circle with radius ' + _radius.get(this));
+// //     }
+// // }
+
+// const Circle = require('./circle');
+
+// const c = new Circle(10);
+// c.draw();
+
+
+// --------------------------------------------------------------------------------
+
+
+
+// ------> code for 'circle2.js' module:
+
+import {Circle2} from './circle2.js';
+
+const c = new Circle2(10);
+c.draw;
+
+// --------------------------------------------------------------------------------
